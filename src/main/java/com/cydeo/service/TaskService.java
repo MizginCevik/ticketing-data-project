22fcf23382fd0dc31bea515615ec78427fcf781b
mizgin <mizgincvk@gmail.com>
2022-11-06 12:05:21 -0500

    classes related with Task is created

---
+++ src/main/java/com/cydeo/service/TaskService.java
@@ -0,0 +1,15 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.TaskDTO;
+
+import java.util.List;
+
+public interface TaskService {
+
+    void save(TaskDTO dto);
+    void update(TaskDTO dto);
+    void delete(Long id); // we don't have any unique field that's why used id
+    List<TaskDTO> listAllTasks();
+    TaskDTO findById(Long id);
+
+}
