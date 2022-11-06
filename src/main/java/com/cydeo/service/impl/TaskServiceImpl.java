ae612337974a85582c6269f59d01742a218c1d37
mizgin <mizgincvk@gmail.com>
2022-11-06 13:05:17 -0500

    added delete and update implementations

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,6 +1,7 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.entity.Task;
 import com.cydeo.enums.Status;
 import com.cydeo.mapper.TaskMapper;
 import com.cydeo.repository.TaskRepository;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -31,12 +33,24 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public void update(TaskDTO dto) {
+        Optional<Task> task = taskRepository.findById(dto.getId());
+        Task convertedTask = taskMapper.convertToEntity(dto);
 
+        if (task.isPresent()) {
+            convertedTask.setTaskStatus(task.get().getTaskStatus());
+            convertedTask.setAssignedDate(task.get().getAssignedDate());
+            taskRepository.save(convertedTask);
+        }
     }
 
     @Override
     public void delete(Long id) {
+        Optional<Task> foundTask = taskRepository.findById(id);
 
+        if (foundTask.isPresent()) {
+            foundTask.get().setIsDeleted(true);
+            taskRepository.save(foundTask.get());
+        }
     }
 
     @Override
@@ -46,6 +60,11 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public TaskDTO findById(Long id) {
+        Optional<Task> task = taskRepository.findById(id);
+
+        if (task.isPresent()) {
+            return taskMapper.convertToDto(task.get());
+        }
         return null;
     }
 
